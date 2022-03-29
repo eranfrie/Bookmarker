56c40fba54087630c8e65f35e7eb3398bf31c016
Tal Melikson <talfr2@gmail.com>
2022-03-29 13:19:47 +0300

    Display bookmarks

--- src/app/app.py
+++ src/app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from server import bookmarks
 
 
 class App:
@@ -7,7 +8,12 @@ class App:
 
         @self.app.route('/')
         def hello_world():
-            return "EasyBookmarks"
+            all_bookmarks = bookmarks.get_all_bookmarks()
+            bookmarks_html = ""
+            for b in all_bookmarks:
+                bookmarks_html += f"<b>{b.title}:</b> {b.description} " \
+                    f"<a href={b.link} target=\"_blank\">{b.link}</a><br>"
+            return f"<h1>EasyBookmarks</h1> {bookmarks_html}"
 
     def run(self, host, port):
         self.app.run(host=host, port=port)
