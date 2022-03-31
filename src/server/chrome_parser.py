041d9e14ea01f69b4f4e9478ba15d38ca1a96831
Eran Friedman <fr.eran@gmail.com>
2022-03-31 17:32:26 +0300

    Chrome parser: parse Chrome's exported bookmarks

---
+++ src/server/chrome_parser.py
@@ -0,0 +1,32 @@
+from html.parser import HTMLParser
+
+
+class ChromeParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+
+        self.bookmarks = []
+
+        self.a_tag = False
+        self.url = None
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":
+            self.a_tag = True
+            for k, v in attrs:
+                if k == "href":
+                    self.url = v
+
+    def handle_data(self, data):
+        if self.a_tag:
+            self.bookmarks.append(
+                {
+                    "title": data,
+                    "url": self.url,
+                }
+            )
+            self.a_tag = False
+
+    def get_bookmarks(self, html):
+        self.feed(html)
+        return self.bookmarks
