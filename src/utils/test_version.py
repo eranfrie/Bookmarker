00b2e675792d482436ccb85f84be24eb0d5f517d
Eran Friedman <fr.eran@gmail.com>
2022-04-24 07:27:35 +0300

    Split test_version into 2 functions

--- src/utils/test_version.py
+++ src/utils/test_version.py
@@ -4,10 +4,15 @@ from utils import version
 # pylint: disable=R0201
 class TestVersion:
     def test_version(self):
-        # just run it and check it has value
+        """
+        just run it and check it returns (some) value.
+        """
         assert version.get_version()
 
-        # override get_patch() and compare version
+    def test_version_mock_patch(self):
+        """
+        override get_patch() and compare version.
+        """
         patch_func = version.get_patch()
         version.get_patch = lambda: "test_patch"
         assert version.get_version() == f"{version.MAJOR}.{version.MINOR}.test_patch"
