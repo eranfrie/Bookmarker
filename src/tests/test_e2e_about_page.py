b29e6675201ede0b9343698e2172ce77e5a11ad8
Eran Friedman <fr.eran@gmail.com>
2022-04-27 22:20:46 +0300

    E2E tests: test 'About' page - validate version

--- src/tests/test_e2e_about_page.py
+++ src/tests/test_e2e_about_page.py
@@ -1,6 +1,7 @@
 import requests
 
 from tests.test_e2e_base import TestE2eBase, NUM_MENU_LINKS
+from utils import version, opts
 
 
 URL = "http://localhost:8000/about"
@@ -13,3 +14,6 @@ class TestE2eAboutPage(TestE2eBase):
         assert response.status_code == 200
         assert response.text.count("About") == 2  # menu + sub-header
         assert response.text.count("href") == NUM_MENU_LINKS + 1  # 1 for github link
+
+        ver = version.get_version()
+        assert f"{opts.PROD_NAME} Version {ver}" in response.text
