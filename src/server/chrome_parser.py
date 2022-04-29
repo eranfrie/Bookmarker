89a3a4af3481f28e50cb45d1d63eedba21cd5108
Eran Friedman <fr.eran@gmail.com>
2022-04-29 16:14:54 +0300

    Chrome parser: parse sections

--- src/server/chrome_parser.py
+++ src/server/chrome_parser.py
@@ -8,25 +8,65 @@ class ChromeParser(HTMLParser):
 
         self.bookmarks = []
 
+        self.h3_tag = False
+        self.first_h3_tag = True
+        self.sections = []
+
         self.a_tag = False
         self.url = None
 
+    def _get_section(self):
+        section = ""
+        first_section = True
+        for s in self.sections:
+            if first_section:
+                first_section = False
+                section += s
+            else:
+                section += " / " + s
+        return section
+
     def handle_starttag(self, tag, attrs):
+        # bookmark
         if tag == "a":
             self.a_tag = True
             for k, v in attrs:
                 if k == "href":
                     self.url = v
 
+        # new folder
+        if tag == "h3":
+            # ignore first H3 tag
+            if self.first_h3_tag:
+                self.first_h3_tag = False
+            else:
+                self.h3_tag = True
+
+    def handle_endtag(self, tag):
+        # end of folder
+        if tag == "dl":
+            # there are few unrelated DL tags at the beginning
+            # which we ignore when building the section list
+            # so we ignore them at the end too
+            if self.sections:
+                self.sections.pop()
+
     def handle_data(self, data):
+        # bookmark
         if self.a_tag:
+            self.a_tag = False
             self.bookmarks.append(
                 {
+                    "section": self._get_section(),
                     "title": data,
                     "url": self.url,
                 }
             )
-            self.a_tag = False
+
+        # folder
+        if self.h3_tag:
+            self.h3_tag = False
+            self.sections.append(data)
 
     def get_bookmarks(self, html):
         self.feed(html)
--- src/server/test_chrome_parser.py
+++ src/server/test_chrome_parser.py
@@ -3,7 +3,26 @@ from server.chrome_parser import ChromeParser
 
 # pylint: disable=R0201 (no-self-use)
 class TestFuzzySearch:
-    def test_invalid_html(self):
+    def test_chrome_bookmarks(self):
+        with open("src/tests/chrome_bookmarks_test.html", "r", encoding="ascii") as f:
+            html_data = f.read()
+
+        expected_bookmarks = [
+            {"section": "", "title": "n1"},
+            {"section": "s1", "title": "n1_1"},
+            {"section": "s1", "title": "n1_2"},
+            {"section": "s1 / s1_2", "title": "n1_2_1"},
+            {"section": "", "title": "n2"},
+            {"section": "s2", "title": "n2_1"},
+        ]
+
+        bookmarks = ChromeParser().get_bookmarks(html_data)
+
+        for b, e in zip(bookmarks, expected_bookmarks):
+            assert b["section"] == e["section"]
+            assert b["title"] == e["title"]
+
+    def test_chrome_bookmarks_invalid_html(self):
         with open("src/tests/chrome_bookmarks_invalid.html", "r", encoding="ascii") as f:
             html_data = f.read()
 
